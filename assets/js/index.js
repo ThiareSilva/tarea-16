5c4e05d03f54f97ee4a128ea5dba38f60df304d3
Thiare Silva <thiaresilva2552@gmail.com>
2023-11-07 19:44:07 -0300

    Se agrego grafico

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,19 @@ const desplegable = document.getElementById("desplegable");
 const btnBuscar = document.getElementById("btnBuscar");
 const resultadoFinal = document.getElementById("resultadoFinal");
 
+const ctx = document.getElementById("myChart").getContext("2d");
+let chart = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [],
+  },
+  options: {
+    width: "100%",
+    height: "100%"
+  },
+});
+
 async function monedas() {
   try {
     const apiMonedas = await fetch("https://mindicador.cl/api/");
@@ -22,16 +35,57 @@ async function monedas() {
     return apiJson;
   } catch (e) {
     console.log(e);
-    resultadoFinal.innerHTML = (e)
+    resultadoFinal.innerHTML = e;
   }
 }
 
-function convertirMoneda(){
+async function obtenerHistorialMoneda() {
+  const codigoMonedaSeleccionada =
+    desplegable.options[desplegable.selectedIndex].id;
+  try {
+    const res = await fetch(
+      `https://mindicador.cl/api/${codigoMonedaSeleccionada}`
+    );
+    const historialMoneda = await res.json();
 
+    const labels = historialMoneda.serie.map((element) => {
+      return element.fecha;
+    });
 
-    const indiceDeOptionElegidaEnMenuDesplegable = desplegable.selectedIndex;
-    const optionCompletoSeleccionadoEnElMenuDesplegable = desplegable.options[indiceDeOptionElegidaEnMenuDesplegable];
-    const valorMonedaSeleccionada = optionCompletoSeleccionadoEnElMenuDesplegable.value;
-    resultadoFinal.innerHTML = (Number(moneditas.value) / Number(valorMonedaSeleccionada)).toString();
+    const data = historialMoneda.serie.map((element) => {
+      return element.valor;
+    });
+
+    const datasets = [
+      {
+        label:
+        desplegable.options[desplegable.selectedIndex]
+            .innerHTML,
+        borderColor: "black",
+        data,
+      },
+    ];
+
+    // Update the chart's data
+    chart.data.labels = labels;
+    chart.data.datasets = datasets;
+
+    // Update the chart
+    chart.update();
+  } catch (e) {
+    console.log(e.error);
+  }
+}
+
+function convertirMoneda() {
+  const indiceDeOptionElegidaEnMenuDesplegable = desplegable.selectedIndex;
+  const optionCompletoSeleccionadoEnElMenuDesplegable =
+    desplegable.options[indiceDeOptionElegidaEnMenuDesplegable];
+  const valorMonedaSeleccionada =
+    optionCompletoSeleccionadoEnElMenuDesplegable.value;
+  resultadoFinal.innerHTML = (
+    Number(moneditas.value) / Number(valorMonedaSeleccionada)
+  ).toString();
+  obtenerHistorialMoneda();
 }
 monedas();
